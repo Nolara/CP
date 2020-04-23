8bcdffc46646ba971f4154be7a9f4e5e77b6c809
Lara Nollen <Lara.Nollen@tu-dortmund.de>
2020-04-23 14:07:29 +0200

    Add exercise 3b and some improvement

--- Blatt0/Aufgabe3.cpp
+++ Blatt0/Aufgabe3.cpp
@@ -1,48 +1,73 @@
 #include <iostream>
 #include <math.h> // exp-Funktion
 #include <fstream> // Ausgabe als txt
+#include <vector>
 using namespace std;
 
-
-void euler_normal(int n, double y_0, double delta_t, double* y){ // Normales Eulerverfahren
-  y[0] = y_0; //Startwertübergabe
-  for(int i=1; i<=n; i++){
-    y[i] = y[i-1]*(1-delta_t); //Rekursion
-  }
+vector<double> euler_normal(int n, double y_0, double delta_t){
+    vector<double> y;
+    y.push_back(y_0);
+    for(int i=1; i<=n; i++){
+        y.push_back(y[i-1]*(1-delta_t));
+    }
+    return y;
 }
 
-
-void euler_symm(int n, double y_0, double delta_t, double* y){
-  y[0] = y_0; // Startwertübergabe
-  y[1] = exp(-delta_t);
-  for(int i=2; i<=n; i++){
-    y[i] = -2*delta_t*y[i-1]+y[i-2]; //Rekursion
-  }
+vector<double> euler_symm(int n, double y_0,  double y_1, double delta_t){
+    vector<double> y;
+    y.push_back(y_0);
+    y.push_back(y_1);
+    for(int i=2; i<=n; i++){
+        y.push_back(-2*delta_t*y[i-1]+y[i-2]);
+    }
+    return y;
 }
-
 
 int main(){
 
   cout << "Start Aufgabe 3" << endl;
-  double y_0 = 1.0; // Festlegung des Startwerts
+
   double delta_t = 0.1  ; // Festlegung der Schrittweite
+  double y_0a = 1.0; // Festlegung des Startwerts
+  double y_0b = 1.0-delta_t; // Festlegung des Startwerts
+  double y_1a = exp(-delta_t); // Festlegung des Startwerts
+  double y_1b = y_0a-delta_t; // Festlegung des Startwerts
   int n = 10/delta_t; // Anzahl der Schritte
+
   cout << "Anzahl der Schritte beträgt " << n << endl;
-  double y_normal[n]; //Werte des normalen Eulerverfahrens
-  euler_normal(n, y_0, delta_t, y_normal); // Aufruf des normalen Eulerverfahrens
-  double y_symm[n]; //Werte des symmetrischen Eulerverfahrens
-  euler_symm(n, y_0, delta_t, y_symm); // Aufruf des symmetrischen Eulerverfahrens
+
+  vector<double> y_a_normal;
+  y_a_normal = euler_normal(n, y_0a, delta_t); // Aufruf des normalen Eulerverfahrens
+
+  vector<double> y_a_symm;
+  y_a_symm = euler_symm(n, y_0a, y_1a, delta_t); // Aufruf des normalen Eulerverfahrens
+
+  vector<double> y_b_normal;
+  y_b_normal = euler_normal(n, y_0b, delta_t); // Aufruf des normalen Eulerverfahrens
+
+  vector<double> y_b_symm;
+  y_b_symm = euler_symm(n, y_0a, y_1b, delta_t); // Aufruf des normalen Eulerverfahrens
 
 
-  ofstream file ("Data/three.txt", std::ofstream::out);
-  file << "# t=n*delta_t euler_normal euler_symmetrisch analytisch" << endl;
+  ofstream afile ("Data/three_a.txt", std::ofstream::out);
+  afile << "# t=n*delta_t euler_normal euler_symmetrisch analytisch" << endl;
   for(int i=0; i<=n; i++){
-    file << i*delta_t << " ";
-    file << y_normal[i] << " ";
-    file << y_symm[i] << " ";
-    file << exp(-i*delta_t) << endl; // Analytische Lösung
+    afile << i*delta_t << " ";
+    afile << y_a_normal[i] << " ";
+    afile << y_a_symm[i] << " ";
+    afile << exp(-i*delta_t) << endl; // Analytische Lösung
   }
-  file.close();
+  afile.close();
+
+  ofstream bfile ("Data/three_b.txt", std::ofstream::out);
+  bfile << "# t=n*delta_t euler_normal euler_symmetrisch analytisch" << endl;
+  for(int i=0; i<=n; i++){
+    bfile << i*delta_t << " ";
+    bfile << y_b_normal[i] << " ";
+    bfile << y_b_symm[i] << " ";
+    bfile << exp(-i*delta_t) << endl; // Analytische Lösung
+  }
+  bfile.close();
 
   cout << "Aufgabe 3 ist abgeschlossen" << endl;
   return 0;
