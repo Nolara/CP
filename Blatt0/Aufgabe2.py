e3496db8ed34d3b8746d7b95033fd87d00119c88
Lara Nollen <Lara.Nollen@tu-dortmund.de>
2020-04-23 12:02:57 +0200

    Fixed exercise two

--- Blatt0/Aufgabe2.py
+++ Blatt0/Aufgabe2.py
@@ -5,49 +5,72 @@ import matplotlib.pyplot as plt
 from uncertainties import ufloat
 from scipy.stats import sem
 
-xa, ya = np.genfromtxt('./Data/two_a.txt', unpack=True) #import data
-xb, yb = np.genfromtxt('./Data/two_b.txt', unpack=True) #import data
-xc, yc = np.genfromtxt('./Data/two_c.txt', unpack=True) #import data
+xa, ya, za = np.genfromtxt('./Data/two_a.txt', unpack=True) #import data
+xb, yb, zb = np.genfromtxt('./Data/two_b.txt', unpack=True) #import data
+xc, yc, zc = np.genfromtxt('./Data/two_c.txt', unpack=True) #import data
+delta = np.genfromtxt('./Data/two_delta.txt', unpack=True) #import data
 
-def f(x):
-    return 1/(x)**(1/2)-1/(x+1)**(1/2)
-
-xl=np.linspace(1000000,10000000, 50000)
+arel=abs(ya-za)/za*100
+brel=abs(yb-zb)/zb*100
+crel=abs(yc-zc)/zc*100
 
 plt.figure(1)
-plt.plot (xa, ya, 'k-', label='Funktion a mit Auslöschung')
-#plt.plot (xl, f(xl), 'r-', label='Exakter Wert')
+plt.plot (xa, ya, 'r-', label='Funktion a mit Auslöschung')
+plt.plot (xa, za, 'k-', label='Funktion a ohne Auslöschung')
+plt.xscale('log')
 plt.yscale('log')
 plt.grid()
-plt.xlabel(r't')
-#plt.ylabel(r'')
+plt.xlabel(r'x')
+plt.ylim(10e-12, 2e-8)
 plt.legend(loc="best")
 plt.tight_layout()
 plt.savefig('Plots/2a.pdf')
-#plt.show()
 
 plt.figure(2)
 plt.plot (xb, yb, 'r-', label='Funktion b mit Auslöschung')
-plt.plot (xb, ybo, 'k-', label='Funktion b ohne Auslöschung')
-#plt.plot (xl, f(xl), 'r-', label='Exakter Wert')
+plt.plot (xb, zb, 'k-', label='Funktion b ohne Auslöschung')
 plt.grid()
-plt.yscale('log')
+plt.xscale('log')
 plt.xlabel(r'x')
-#plt.ylabel(r'')
 plt.legend(loc="best")
 plt.tight_layout()
 plt.savefig('Plots/2b.pdf')
-plt.show()
 
 plt.figure(3)
+plt.title('$\delta$ = '+str(delta))
 plt.plot (xc, yc, 'r-', label='Funktion c mit Auslöschung')
-#plt.plot (xc, yco, 'k-', label='Funktion b ohne Auslöschung')
-#plt.plot (xl, f(xl), 'r-', label='Exakter Wert')
+plt.plot (xc, zc, 'k-', label='Funktion c ohne Auslöschung')
 plt.grid()
-plt.yscale('log')
 plt.xlabel(r'x')
-#plt.ylabel(r'')
 plt.legend(loc="best")
 plt.tight_layout()
 plt.savefig('Plots/2c.pdf')
-plt.show()
+
+plt.figure(4)
+plt.plot (xa, arel, 'r-', label='Relativer Fehler')
+plt.grid()
+plt.xlabel(r'x')
+plt.xscale('log')
+plt.ylabel(r'Fehler in %')
+plt.legend(loc="best")
+plt.tight_layout()
+plt.savefig('Plots/2arel.pdf')
+
+plt.figure(5)
+plt.plot (xb, brel, 'r-', label='Relativer Fehler')
+plt.grid()
+plt.xlabel(r'x')
+plt.xscale('log')
+plt.ylabel(r'Fehler in %')
+plt.legend(loc="best")
+plt.tight_layout()
+plt.savefig('Plots/2brel.pdf')
+
+plt.figure(6)
+plt.plot (xc, crel, 'r-', label='Relativer Fehler')
+plt.grid()
+plt.xlabel(r'x')
+plt.ylabel(r'Fehler in %')
+plt.legend(loc="best")
+plt.tight_layout()
+plt.savefig('Plots/2crel.pdf')
--- Blatt0/Aufgabe3.py
+++ Blatt0/Aufgabe3.py
@@ -7,6 +7,9 @@ from scipy.stats import sem
 
 dt, en, es, an = np.genfromtxt('./Data/three.txt', unpack=True) #import data
 
+en_rel=abs(an-en)/an*100 #calculate the relative error
+es_rel=abs(an-es)/an*100
+
 plt.figure(1)
 plt.plot (dt, en, 'k-', label='Normales Eulerverfahren')
 plt.plot (dt, es, 'r-', label='Symmetrisches Eulerverfahren')
@@ -14,23 +17,18 @@ plt.plot (dt, an, 'g-', label='Analytisches Ergebnis')
 plt.yscale('log')
 plt.grid()
 plt.xlabel(r't')
-#plt.ylabel(r'')
 plt.legend(loc="best")
 plt.tight_layout()
 plt.savefig('Plots/3.pdf')
-#plt.show()
 
 plt.figure(2)
-en_rel=abs(an-en)/an*100 #calculate the relative error
-es_rel=abs(an-es)/an*100
 plt.plot (dt, en_rel, 'k-', label='Relativer Fehler des normalen Eulerverfahren')
 plt.plot (dt, es_rel, 'r-', label='Relativer Fehler des symmetrischen Eulerverfahren')
 plt.yscale('log')
 plt.xlim(0,10)
 plt.grid()
 plt.xlabel(r't')
-plt.ylabel(r'Abweichung in %')
+plt.ylabel(r'Fehler in %')
 plt.legend(loc="best")
 plt.tight_layout()
 plt.savefig('Plots/3rel.pdf')
-#plt.show()
