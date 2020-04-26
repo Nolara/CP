8d94d44ac66a0a5478ee2143396066e3a3401b7e
Lara Nollen <Lara.Nollen@tu-dortmund.de>
2020-04-26 19:53:59 +0200

    Add comments

--- Blatt1/Aufgabe2.cpp
+++ Blatt1/Aufgabe2.cpp
@@ -9,32 +9,33 @@ using namespace Eigen;
 int main()
 {
 
+
    VectorXf x(10);
-   x <<0, 2.5, -6.3, 4, -3.2, 5.3, 10.1, 9.5, -5.4, 12.7;
+   x <<0, 2.5, -6.3, 4, -3.2, 5.3, 10.1, 9.5, -5.4, 12.7; //x aus der Aufgabenstellung
 
    VectorXf a(10);
-   a <<1, 1, 1, 1, 1, 1, 1, 1, 1, 1;
+   a <<1, 1, 1, 1, 1, 1, 1, 1, 1, 1; //Hilfsvektor a
 
    MatrixXf A(10,2);
-   A << x, a;
+   A << x, a; //Erstelle Matrix aus x und a
 
    VectorXf y(10);
-   y <<4, 4.3, -.9, 6.5, 0.7, 8.6, 13, 9.9, -3.6, 15.1;
+   y <<4, 4.3, -.9, 6.5, 0.7, 8.6, 13, 9.9, -3.6, 15.1; //y aus der Aufgabenstellung
 
    MatrixXf AT(2,10);
-   AT << A.transpose();
+   AT << A.transpose(); //Transponiere A
 
    MatrixXf ATA(2,2);
-   ATA << AT*A;
+   ATA << AT*A; //Produkt aus A transponiert und A
 
    VectorXf ATy(2);
-   ATy << AT*y;
+   ATy << AT*y; //Produkt aus A transponiert und y
 
    Vector2f x0;
-   x0= ATA.partialPivLu().solve(ATy);
+   x0= ATA.partialPivLu().solve(ATy); //Löse ATAx= ATy
 
-   ofstream afile ("Data/two_data.txt", std::ofstream::out); // Erstelle txt Datei für Aufgabenteil a
-   ofstream bfile ("Data/two_x0.txt", std::ofstream::out); // Erstelle txt Datei für Aufgabenteil b
+   ofstream afile ("Data/two_data.txt", std::ofstream::out); // Erstelle txt Datei für Daten aus Aufgabenstellung
+   ofstream bfile ("Data/two_x0.txt", std::ofstream::out); // Erstelle txt Datei Lösungsvektor
    afile << "# x y" << "\n";
    bfile << "# m n" << "\n";
    int n;
