da0116cb2ee4f8edc247e45f12a36533bde1fee2
Lara Nollen <Lara.Nollen@tu-dortmund.de>
2020-05-02 18:28:06 +0200

    Aufgabe 1 fertig

--- Blatt2/Aufgabe1.cpp
+++ Blatt2/Aufgabe1.cpp
@@ -1,224 +1,94 @@
-#include <iostream>
-#include <Eigen/Dense>
-#include <math.h>
 #include <fstream>
+#include <iostream>
+#include <vector>
+#include <Eigen/Dense>
+#include <Eigen/SVD>
+
 
 using namespace std;
 using namespace Eigen;
 
-int main()
+
+int loadData(Eigen::MatrixXd &mat, std::string filename, size_t row, size_t col)
 {
 
-   Vector3d a1;
-   Vector3d a2;
-   Vector3d a3;
+	std::ifstream input;
+	input.open(filename, std::fstream::in | std::fstream::binary);
+	if (input.is_open())
+	{
+		std::vector<unsigned char> vec((std::istreambuf_iterator<char>(input)),
+				std::istreambuf_iterator<char>() );
+		mat= Eigen::Map<Eigen::Matrix<unsigned char, Eigen::Dynamic,Eigen::Dynamic>>(vec.data(),row,col).template cast<double>();
+		input.close();
+		return 1;
+	}
+	return 0;
 
-   //Eingabe der Vektoren aus der Aufgabenstellung
-   a1 <<0.5, sqrt(3)/2, 0;
-   a2 <<-0.5, sqrt(3)/2, 0;
-   a3 <<0, 0, 1;
+}
+int storeData(Eigen::MatrixXd &mat, std::string filename)
+{
+	Eigen::Matrix<char, Eigen::Dynamic, Eigen::Dynamic> pic= mat.template cast<char>();
+	std::ofstream output;
+	output.open(filename,std::fstream::trunc);
+	if (output.is_open())
+	{
+		output.write(pic.data(),pic.size());
+		output.close();
+		return 1;
+	}
+	return 0;
+}
 
-   Vector3d b;
-   b << 2, 0, 2;
+string name( const string& basename, float index, const string& ext)
+{
+	ostringstream name;
+	name << basename << index << ext;
+	return name.str();
+}
 
-   //Definiere Matrix aus Basisvektoren
-   Matrix3d A;
-   A << a1, a2, a3;
+void Reduktion(int k, MatrixXd Singular, MatrixXd U, MatrixXd V, int n)
+{
+    MatrixXd Reduktion(n, n);
 
-   //Lösen mit LU Zerlegung
-   Vector3d x;
-   x= A.partialPivLu().solve(b);
-
-   //Bestimme Matrix Pivotisierungsmatrix P
-   Matrix3d P;
-   P = A.partialPivLu().permutationP();
-
-   //Bestimme Matrix L*U
-   Matrix3d LU;
-   LU=A.partialPivLu().matrixLU();
-
-   //Teile L*U in L und U auf
-   Matrix3d L;
-   L=LU.triangularView<StrictlyLower>();
-   for(int i=0; i<=2; i++){ //Setze Diagonale =1
-     L(i,i)=1;
-   }
-
-   Matrix3d U;
-   U=LU.triangularView<Upper>();
-
-   //Eingabe des Vektors aus Aufgabenteil c)
-   Vector3d b2;
-   b2 << 1, 2*sqrt(3), 3;
-
-   //Multipliziere b mit Pivotisierungsmatrix P
-   Vector3d pb;
-   pb=P*b2;
-
-   //Erstelle Vektor zum Speichern der Zwischenergebnisse aus der Forward Substitution
-   Vector3d x_zwischen;
-
-   //Erster Wert der Forward Substitution
-   x_zwischen(0)=pb(0)/L(0,0);
-
-   //Forward Substitution
-   for(int i = 1; i < 3; i++)
-   {
-       float s = 0;
-       for(int j = 0; j < i; j++)
-       {
-           s = s + L(i,j)*x_zwischen(j);
-       }
-       x_zwischen(i) = (pb(i) - s)/L(i,i);
-   }
-
-   //Definiere Lösungsvektor für c)
-   Vector3d x2;
-
-   //Erster Wert der Backward Substitution
-   x2(2)=x_zwischen(2)/U(2,2);
-
-   //Backward Substitution
-   for (int i = 1; i >=0; i--)
-   {
-       double s = 0;
-       for (int j = i+1; j <3; ++j)
-       {
-           s = s + U(i,j)*x2(j);;
-       }
-       x2(i)=(x_zwischen(i)-s)/U(i,i);
-   }
-
-
-   //Umgestellte Matrix aus Aufgabenteil d)
-   Matrix3d A2;
-   A2 << a3, a2, a1;
-
-   //Löse d) mit LU
-   Vector3d xd;
-   xd= A2.partialPivLu().solve(b);
-
-   //Pivotisierungsmatrix P aus d)
-   Matrix3d P2;
-   P2 = A2.partialPivLu().permutationP();
-
-   //Matrix LU aus d)
-   Matrix3d LU2;
-   LU2=A2.partialPivLu().matrixLU();
-
-   //Matrix L aus d)
-   Matrix3d L2;
-   L2=LU2.triangularView<StrictlyLower>();
-   for(int i=0; i<=2; i++)
-   {
-       L2(i,i)=1;
-   }
-
-   //Matrix U aus d)
-   Matrix3d U2;
-   U2=LU2.triangularView<Upper>();
-
-   //Schreibe Werte in txt Datei
-   ofstream afile ("Data/one.txt", std::ofstream::out); // Erstelle txt Datei für Aufgabenteil a
-   afile << "# Aufgabenteil b:" << "\n" <<  "\n";
-   afile << "# Lösungsvektor:" << "\n";
-   int n_b;
-   n_b=x.size(); //Länge Vektor x
-   for(int i=0; i<n_b; i++){
-       afile << x[i] << "\n"; //Schreibe x in txt Datei
-   }
-   afile << "\n" << "# Matrix P:" << "\n";
-
-   //Größe der Matrizen
-   int r=P.rows();;
-   int c=P.cols();
-
-   //Schreibe P in txt Datei
-   for (int i = 0; i < r; ++i)
-   {
-       for (int j = 0; j < c; ++j)
-       {
-           afile << P(i,j) << "\t";
-       }
-       afile << "\n";
-   }
-
-   //Schreibe L in txt Datei
-    afile << "\n" << "# Matrix L:" << "\n";
-
-    for (int i = 0; i < r; ++i)
+    for (float i = 0; i < k; i++)
     {
-        for (int j = 0; j < c; ++j)
-        {
-            afile << L(i,j) << "\t";
-        }
-        afile << "\n";
+    	Reduktion += Singular(i) * U.col(i) * V.row(i);
     }
 
-    //Schreibe U in txt Datei
-    afile << "\n" << "# Matrix U:" << "\n";
+    ofstream file;
+    file.open (name("Data/Reduktion_", k, ".txt"), ofstream::out);
+    file << Reduktion.transpose();
+    file.close();
+    Reduktion.resize(0,0);
+}
 
-    for (int i = 0; i < r; ++i)
-    {
-        for (int j = 0; j < c; ++j)
-        {
-            afile << U(i,j) << "\t";
-        }
-        afile << "\n";
-    }
+int main()
+{
+    int size = 512;
+    MatrixXd M(size, size);
+    loadData(M, "Bild", size, size);
+    MatrixXd Original(size, size);
+    Original = M.transpose();
 
-    afile << "\n" << "# Aufgabenteil c:" << "\n" << "\n";
-    afile << "# Lösungsvektor:" << "\n";
+    BDCSVD<MatrixXd> svd(M, ComputeFullV | ComputeFullU );
 
-    //Schreibe x aus c) in txt Datei
-    for(int i=0; i<n_b; i++){
-        afile << x2[i] << "\n";
-    }
+    MatrixXd Singular(size, size);
+    Singular=svd.singularValues();
 
-    afile << "\n" <<  "# Aufgabenteil d:" << "\n" << "\n";
-    afile << "# Lösungsvektor:" << "\n";
+    MatrixXd U(size, size);
+    U=svd.matrixU();
 
-    //Schreibe x aus d) in txt Datei
-    for(int i=0; i<n_b; i++){
-        afile << xd[i] << "\n";
-    }
-    afile << "\n" << "# Matrix P:" << "\n";
-
-    //Schreibe P aus d) in txt Datei
-    for (int i = 0; i < r; ++i)
-    {
-        for (int j = 0; j < c; ++j)
-        {
-            afile << P2(i,j) << "\t";
-        }
-        afile << "\n";
-    }
-
-    //Schreibe L aus d) in txt Datei
-     afile << "\n" << "# Matrix L2:" << "\n";
-
-     for (int i = 0; i < r; ++i)
-     {
-         for (int j = 0; j < c; ++j)
-         {
-             afile << L2(i,j) << "\t";
-         }
-         afile << "\n";
-     }
-
-     //Schreibe U aus d) in txt Datei
-     afile << "\n" << "# Matrix U2:" << "\n";
-
-     for (int i = 0; i < r; ++i)
-     {
-         for (int j = 0; j < c; ++j)
-         {
-             afile << U2(i,j) << "\t";
-         }
-         afile << "\n";
-     }
+    MatrixXd V(size, size);
+    V=svd.matrixV().transpose();
 
 
-   afile.close();
+    ofstream afile ("Data/one_original.txt", std::ofstream::out); // Erstelle txt Datei für Aufgabenteil a
+    afile << "# Aufgabenteil b:" << "\n" <<  "\n";
+    afile << Original << endl;
+    afile.close();
+
+    Reduktion(10, Singular,U, V, size);
+    Reduktion(20, Singular,U, V, size);
+    Reduktion(50, Singular,U, V, size);
 
 }
--- Blatt2/Aufgabe2.cpp
+++ Blatt2/Aufgabe2.cpp
@@ -6,65 +6,55 @@
 using namespace std;
 using namespace Eigen;
 
+MatrixXd Random_matrix(int n){
+    MatrixXd A(n,n);
+    A= MatrixXd::Random(n,n);
+    return A; // Ausgabe des Ergebnis-Vektors
+}
+
+VectorXd Random_vector(int n){
+    VectorXd v(n);
+    v=VectorXd::Random(n);
+    return v; // Ausgabe des Ergebnis-Vektors
+}
+
+
 int main()
 {
+    int n=5;
+    MatrixXd A1=Random_matrix(n);
+    MatrixXd b=Random_vector(n);
 
 
-   VectorXd x(10);
-   x <<0, 2.5, -6.3, 4, -3.2, 5.3, 10.1, 9.5, -5.4, 12.7; //x aus der Aufgabenstellung
+    A1.partialPivLu();
+    VectorXd x(n);
+    x= A1.partialPivLu().solve(b);
 
-   VectorXd a(10);
-   a <<1, 1, 1, 1, 1, 1, 1, 1, 1, 1; //Hilfsvektor a
+    ofstream afile ("Data/two_data.txt", std::ofstream::out);
 
-   MatrixXd A(10,2);
-   A << x, a; //Erstelle Matrix aus x und a
+    afile << "# Matrix A:" << "\n";
 
-   VectorXd y(10);
-   y <<4, 4.3, -3.9, 6.5, 0.7, 8.6, 13, 9.9, -3.6, 15.1; //y aus der Aufgabenstellung
+    for (int i = 0; i < n; ++i) //Schreibe Matrix ATA in txt Datei
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            afile << A1(i,j) << "\t";
+        }
+        afile << "\n";
+    }
 
-   MatrixXd AT(2,10);
-   AT << A.transpose(); //Transponiere A
+    afile  << "\n" << "# Vector b:" << "\n";
 
-   MatrixXd ATA(2,2);
-   ATA << AT*A; //Produkt aus A transponiert und A
+    for(int i=0; i<n; i++){
+        afile << b(i) << "\n"; //Schreibe ATy in txt Datei
+    }
 
-   VectorXd ATy(2);
-   ATy << AT*y; //Produkt aus A transponiert und y
+    afile  << "\n" << "# Vector x:" << "\n";
 
-   Vector2d x0;
-   x0= ATA.partialPivLu().solve(ATy); //Löse ATAx= ATy
+    for(int i=0; i<n; i++){
+        afile << x(i) << "\n"; //Schreibe ATy in txt Datei
+    }
 
-   ofstream afile ("Data/two_data.txt", std::ofstream::out); // Erstelle txt Datei für Daten aus Aufgabenstellung
-   ofstream bfile ("Data/two_x0.txt", std::ofstream::out); // Erstelle txt Datei Lösungsvektor
-   ofstream cfile ("Data/two_matrix.txt", std::ofstream::out); // Erstelle txt Datei für die Zwischenergebnisse
-   afile << "# x y" << "\n";
-   bfile << "# m n" << "\n";
-   int n;
-   n=x.size();
-   for(int i=0; i<n; i++){
-       afile << x[i] << "\t"; //Schreibe Daten in txt Datei
-       afile << y[i] << "\n";
-   }
-   bfile << x0[0] << "\t" << x0[1] << "\n"; //Schreibe Lösungsvektor in txt Datei
-   cfile << "\n" << "# Matrix ATA:" << "\n";
+    afile.close();
 
-   int r=ATA.rows();;
-   int c=ATA.cols();
-
-   for (int i = 0; i < r; ++i) //Schreibe Matrix ATA in txt Datei
-   {
-       for (int j = 0; j < c; ++j)
-       {
-           cfile << ATA(i,j) << "\t";
-       }
-       cfile << "\n";
-   }
-   cfile << "\n" << "# Vektor Aty:" << "\n";
-   for(int i=0; i<r; i++){
-       cfile << ATy[i] << "\n"; //Schreibe ATy in txt Datei
-   }
-
-   afile.close();
-   bfile.close();
-   cfile.close();
 }
