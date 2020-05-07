285bedc55b63fc95c04125ac92f58f38cfec56d5
yviribbeheger <yvonne.ribbeheger@gmail.com>
2020-05-07 18:54:08 +0200

    Update Aufgabe0, Aufgabe1 tex

--- Blatt2/Aufgabe1.cpp
+++ Blatt2/Aufgabe1.cpp
@@ -3,41 +3,12 @@
 #include <vector>
 #include <Eigen/Dense>
 #include <Eigen/SVD>
-
+#include "service.cpp"
 
 using namespace std;
 using namespace Eigen;
 
 
-int loadData(Eigen::MatrixXd &mat, std::string filename, size_t row, size_t col) // Programm zum Einlesen der Daten
-{
-
-	std::ifstream input;
-	input.open(filename, std::fstream::in | std::fstream::binary);
-	if (input.is_open())
-	{
-		std::vector<unsigned char> vec((std::istreambuf_iterator<char>(input)),
-				std::istreambuf_iterator<char>() );
-		mat= Eigen::Map<Eigen::Matrix<unsigned char, Eigen::Dynamic,Eigen::Dynamic>>(vec.data(),row,col).template cast<double>();
-		input.close();
-		return 1;
-	}
-	return 0;
-
-}
-int storeData(Eigen::MatrixXd &mat, std::string filename) //Programm zum Abspeichern der Daten
-{
-	Eigen::Matrix<char, Eigen::Dynamic, Eigen::Dynamic> pic= mat.template cast<char>();
-	std::ofstream output;
-	output.open(filename,std::fstream::trunc);
-	if (output.is_open())
-	{
-		output.write(pic.data(),pic.size());
-		output.close();
-		return 1;
-	}
-	return 0;
-}
 
 //Funktion zum Erstellen des Namens der txt Datzei
 string name( const string& basename, float index, const string& ext)
