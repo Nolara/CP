906bfa5d545d42ad9a43c72af11d29a061bd896f
Lara Nollen <Lara.Nollen@tu-dortmund.de>
2020-05-15 08:56:05 +0200

    Add 1.tex

--- Blatt3/Aufgabe1.py
+++ Blatt3/Aufgabe1.py
@@ -6,9 +6,11 @@ from uncertainties import ufloat
 from scipy.stats import sem
 
 
-d = np.genfromtxt('./Data/one_Dichte.txt') #import data
+d = np.genfromtxt('./Data/one_Dichte.txt') #import Dichtematrix
 #Logarithmieren zur besseren Sichtbarkeit
 dlog=np.log(d)
+
+plt.figure(1)
 plt.tight_layout()
 extent=(1,50,-20,20)
 plt.imshow(dlog, interpolation='nearest', extent=extent, origin='lower')
@@ -17,5 +19,17 @@ cbar.ax.set_yticklabels(['10^(-33)','10^(-22)','10^(-11)', '10^(-1)'])
 plt.xticks(np.arange(0,50,5))
 plt.yticks(np.arange(-20,20+2,2))
 plt.xlabel('i')
-plt.ylabel('epsilon')
+plt.ylabel('$\epsilon$')
 plt.savefig('Plots/1.pdf')
+
+
+E0 = np.genfromtxt('./Data/one_Energie.txt') #import E0
+plt.figure(2)
+
+plt.plot (E0, 'x', label='E$_0$')
+plt.grid()
+plt.xlabel('$\epsilon$')
+plt.ylabel('E')
+plt.legend(loc="best")
+plt.tight_layout()
+plt.savefig('Plots/1_Energie.pdf')
