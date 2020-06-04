1ac4bc7562ec785b0a1c6318dcb313c78607e055
SinBehr <64128884+SinBehr@users.noreply.github.com>
2020-06-04 22:40:09 +0200

    Update Aufgabe2.py

--- Blatt6/Aufgabe2.py
+++ Blatt6/Aufgabe2.py
@@ -1,10 +1,63 @@
 import numpy as np
-from numpy import exp
 from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
-from uncertainties import ufloat
-from scipy.stats import sem
 
-k1,x1,y1,f1,abw1= np.genfromtxt('./Data/2_1.txt', unpack=True) #import data
-k2,x2,y2,f2,abw2= np.genfromtxt('./Data/2_2.txt', unpack=True)
-k3,x3,y3,f3,abw3= np.genfromtxt('./Data/2_3.txt', unpack=True)
+k,x1,x2,f  = np.genfromtxt('./Data/two_a.txt', unpack=True)
+
+plt.plot(k,f,'k.')
+plt.xlabel('k')
+plt.ylabel('f(xmin_k)')
+#plt.ylim(-1,100)
+plt.savefig("Plots/plot_A2_a.pdf")
+#plt.show()
+plt.close()
+
+plt.plot(k,f,'k.')
+plt.xlabel('k')
+plt.ylabel('f(xmin_k)')
+plt.ylim(-1,6)
+plt.axhline(0)
+plt.savefig("Plots/plot_A2_a1.pdf")
+#plt.show()
+plt.close()
+
+k,x1,x2,f  = np.genfromtxt('./Data/two_b.txt', unpack=True)
+
+plt.plot(k,f,'k.')
+plt.xlabel('k')
+plt.ylabel('f(xmin_k)')
+#plt.ylim(-1,100)
+plt.savefig("Plots/plot_A2_b.pdf")
+#plt.show()
+plt.close()
+
+plt.plot(k,f,'k.')
+plt.xlabel('k')
+plt.ylabel('f(xmin_k)')
+plt.ylim(-1,6)
+plt.axhline(0)
+plt.savefig("Plots/plot_A2_b1.pdf")
+#plt.show()
+plt.close()
+
+
+k,x1,x2,f  = np.genfromtxt('./Data/two_c.txt', unpack=True)
+
+plt.plot(k,f,'k.')
+plt.xlabel('k')
+plt.ylabel('f(xmin_k)')
+#plt.ylim(-1,100)
+plt.savefig("Plots/plot_A2_c.pdf")
+#plt.show()
+plt.close()
+
+plt.plot(k,f,'k.')
+plt.xlabel('k')
+plt.ylabel('f(xmin_k)')
+plt.ylim(-1,6)
+plt.axhline(0)
+plt.savefig("Plots/plot_A2_c1.pdf")
+#plt.show()
+plt.close()
+
+
